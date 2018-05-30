cddf066e4c732458f636a0dfca55659adb3acf83
Petr Škoda <skodapetr@gmail.com>
2018-05-30 08:26:17 +0200

    Remove source map from production build

--- build/webpack.production.js
+++ build/webpack.production.js
@@ -6,7 +6,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     "mode": "production",
-    "devtool": "inline-source-map",
     "module": {
         "rules": [
             {
