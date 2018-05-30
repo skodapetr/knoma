61a7429f90d071f400cdbbb5a24a280aeec38812
Petr Škoda <skodapetr@gmail.com>
2018-05-30 08:03:20 +0200

    Update component to include some code and css

--- client/app.vue
+++ client/app.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    <h1>3'rd Hello World!</h1>
-  </div>
+    <div class="full-width center-content">
+        <h1>Hello World!</h1>
+    </div>
 </template>
+
+<script>
+    console.log("1,2,3");
+    if (false) {
+        console.log("dead code");
+    }
+</script>
+
+
+<style scoped>
+    .full-width {
+        width: 100%;
+    }
+    .center-content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
