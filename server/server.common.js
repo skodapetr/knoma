25dd1de5bf0ac4581790965175b620fe6e127919
Petr Škoda <skodapetr@gmail.com>
2018-06-10 20:44:58 +0200

    Remove body-parser from global use

--- server/server.common.js
+++ server/server.common.js
@@ -1,15 +1,5 @@
-const path = require("path");
-
 function initializeApi(app) {
-    initializeParsers(app);
-}
-
-function initializeParsers(app) {
-    const bodyParser = require("body-parser");
-    // Parse application/x-www-form-urlencoded.
-    app.use(bodyParser.urlencoded({"extended": false}));
-    // Parse application/json.
-    app.use(bodyParser.json());
+    // Nothing to do here, for now.
 }
 
 function start(app) {
