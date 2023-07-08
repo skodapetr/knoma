a525e3894dd42c736c5e766802653781a9c636e8
Petr Škoda <skodapetr@gmail.com>
2023-07-08 17:05:30 +0200

    Add script to migrate databases

---
+++ cli/migrate-file-v1-to-directory-v1-database.js
@@ -0,0 +1,24 @@
+const logger = require("../server/logging");
+
+const fileDatabase = require("../server/database/file-v1");
+const directoryDatabase = require("../server/database/directory-v1");
+
+(async function main() {
+  await fileDatabase.initialize();
+  await directoryDatabase.initialize();
+  const documents = await fileDatabase.listDocuments();
+  logger.info("Converting documents.", {count: documents.length});
+  for (const document of documents) {
+    const fullDocument = await fileDatabase.loadDocument(document.iri);
+    updateDocument(fullDocument);
+    await directoryDatabase.storeDocument(fullDocument);
+  }
+})();
+
+/**
+ * Replace "iri" with identifier, we construct IRI using instance base URL later.
+ */
+function updateDocument(document) {
+  const iri = document.iri;
+  document.identifier = iri.substring(iri.lastIndexOf("/") + 1);
+}
